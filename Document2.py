11fb2f95b47a4bb3da6c953c67809b53f669cc7c
himopantsu <57516419+himopantsu@users.noreply.github.com>
2020-01-26 00:50:44 +0900

    Update Document2.py

--- Document2.py
+++ Document2.py
@@ -58,7 +58,7 @@ async def on_ready():
 	
 @client.event
 async def on_message(message):
-	print(message.attachments)
+	print(message.attachments.url)
 	"""メッセージを処理"""
 	if message.author.bot:  # ボットのメッセージをハネる
 		return
